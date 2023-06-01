cb9b2ed006c8ddf586225b1c7f53f71d75837936
usmanTheCodder <kh.usman444@gmail.com>
2023-06-01 21:17:00 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/dist"));
+  app.use(express.static("../client/dist"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   });
