c0a733aade5b8933712876a38d8821a0e7211551
usmanTheCodder <kh.usman444@gmail.com>
2023-06-03 11:14:29 +0500

    hagaip

--- server/index.js
+++ server/index.js
@@ -41,14 +41,6 @@ app.get("/", (req, res) => {
 });
 
 
-const URI = process.env.MONGO_URL;
-mongoose.connect(
-  URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-).then(()=> console.log("Connected to mongoDB"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/dist"));
@@ -57,7 +49,17 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+const URI = process.env.MONGO_URL;
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then(()=>{
+  console.log("Connected to mongoDB")
+  app.listen(process.env.PORT, () => {
+    console.log("Server is running on port", process.env.PORT);
+  });
+
 });
